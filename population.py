32be19bb112d59e98c707ad24edd53d7be734c06
Gab0 <gabriel_scf@hotmail.com>
2016-12-11 04:00:45 -0200

    .

---
+++ population.py
@@ -0,0 +1,22 @@
+import sys
+
+import os
+
+from evchess_evolve.core import populate, setmachines
+
+
+if not os.path.isdir("machines"):
+    os.mkdir("machines")
+
+
+
+if for ARG in sys.argv:
+    if ARG == "populate":
+        try:
+            Q = int(sys.argv[ARG+1])
+        except:
+            Q = 16
+        print(Q)
+        machines = populate([], Q, 1)
+        print(len(machines))
+        setmachines(machines, 'machines')
